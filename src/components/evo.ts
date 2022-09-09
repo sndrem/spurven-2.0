7b8f125a8bf86ffc846e900d791f9eeae9bb942c
Sindre Moldeklev <sndrem@gmail.com>
2022-09-09 19:01:36 +0200

    Info om initializing

--- src/components/evo.ts
+++ src/components/evo.ts
@@ -1,6 +1,7 @@
 import evoService from "../services/evoService";
 
 export const evo = (app: any) => {
+  console.log("Initializing Evo-component...");
   app.command("/evo", async ({ack, say}: any) => {
     await ack();
     await say("Sjekker kapasitet hos Evo Bryn...");
